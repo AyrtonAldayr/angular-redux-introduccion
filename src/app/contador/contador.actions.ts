0c00be2475b7b3548dcc8ba4819d82299d4672b4
AyrtonAldayrNevesEstrada <ayrton.24.6.95@gmail.com>
2020-03-29 19:59:27 -0500

    reducer with switch

---
+++ src/app/contador/contador.actions.ts
@@ -0,0 +1,6 @@
+import { createAction } from '@ngrx/store';
+
+export const incrementar = createAction('[Contador] Incrementar');
+export const decrementar = createAction('[Contador] Decrementar');
+//export const decrement = createAction('[Counter Component] Decrement');
+//export const reset = createAction('[Counter Component] Reset');
