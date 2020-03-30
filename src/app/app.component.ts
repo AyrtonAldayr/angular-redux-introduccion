28e0bfc380ae0935e7507f2286508ec19f2e3947
AyrtonAldayrNevesEstrada <ayrton.24.6.95@gmail.com>
2020-03-29 19:59:16 -0500

    reducer with switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actions from './contador/contador.actions';
+
+interface AppState {
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,14 +14,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'redux-app';
   contador: number;
-  constructor() {
-    this.contador = 10;
+  constructor(private store: Store<AppState>) {
+    //this.contador = 10;
+    this.store.select('contador').subscribe(contador => {
+      console.log(contador);
+      this.contador = contador;
+    });
   }
 
   incrementar() {
-    this.contador++;
+    //this.contador++;
+    this.store.dispatch(actions.incrementar());
   }
   decrementar() {
-    this.contador--;
+    //this.contador--;
+    this.store.dispatch(actions.decrementar());
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
-
+import { StoreModule } from '@ngrx/store';
+import { contadorReducer } from './contador/contador.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +17,9 @@ import { NietoComponent } from './contador/nieto/nieto.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ contador: contadorReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
